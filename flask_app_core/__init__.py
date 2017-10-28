99217598867b5b2e60b15e23b09793427e40091f
Jeffrey Marvin Forones <aiscenblue@gmail.com>
2017-10-28 17:52:01 +0800

    configurable configuration

--- flask_app_core/__init__.py
+++ flask_app_core/__init__.py
@@ -10,15 +10,18 @@ class Bootstrap:
 
     __app = None
     __root_dir = None
+    _config = Config
 
-    def __init__(self, main_dir, *args):
+    def __init__(self, import_name, main_dir, *args, **kwargs):
         self.__root_dir = os_path.dirname(main_dir)
-        self.__app = Flask(__name__, instance_relative_config=True)
+        self.__app = Flask(import_name, instance_relative_config=True)
+        if "config" in kwargs:
+            self._config = kwargs["config"]
 
         if len(args):
             self.system_config(system=args[0])
 
-        self.configuration(Config)
+        self.configuration(self._config)
 
     def system_config(self, system):
         if len(system) > 1:
@@ -34,3 +37,4 @@ class Bootstrap:
         """ for blueprint registration """
         Core(app=self.__app, root_path='.module')
         self.__app.run(host=Config.HOST, port=Config.PORT)
+        return self.__app
