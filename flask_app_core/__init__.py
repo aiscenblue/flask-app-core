e3d79bcc10a5d2b8b7bbf135cd79c007e7f70a2e
Jeffrey Marvin Forones <aiscenblue@gmail.com>
2017-10-28 18:08:36 +0800

    bugfix

--- flask_app_core/__init__.py
+++ flask_app_core/__init__.py
@@ -1,8 +1,6 @@
 from os import path as os_path
-
 from flask import Flask
-
-from .config.app import DevelopmentConfig as Config
+from .base_config import DevelopmentConfig as Config
 from flask_blueprint import Core
 
 
@@ -11,12 +9,15 @@ class Bootstrap:
     __app = None
     __root_dir = None
     _config = Config
+    _module_dir = ".module"
 
     def __init__(self, import_name, main_dir, *args, **kwargs):
         self.__root_dir = os_path.dirname(main_dir)
         self.__app = Flask(import_name, instance_relative_config=True)
         if "config" in kwargs:
             self._config = kwargs["config"]
+        if "module" in kwargs:
+            self._module_dir = kwargs["module"]
 
         if len(args):
             self.system_config(system=args[0])
@@ -35,6 +36,6 @@ class Bootstrap:
 
     def start(self):
         """ for blueprint registration """
-        Core(app=self.__app, root_path='.module')
+        Core(app=self.__app, root_path=self._module_dir)
         self.__app.run(host=Config.HOST, port=Config.PORT)
         return self.__app
--- setup.py
+++ setup.py
@@ -1,17 +1,14 @@
 from distutils.core import setup
 
-# this grabs the requirements from requirements.txt
-REQUIREMENTS = [i.strip() for i in open("requirements.txt").readlines()]
-
 setup(
     name='flask-app-core',
-    version='1.0.0',
+    version='1.0.1',
     description='Flask app core',
     author='Jeffrey Marvin Forones',
     author_email='aiscenblue@gmail.com',
     url='https://github.com/aiscenblue/flask-app-core',
     packages=['flask_app_core'],
-    keywords=['flask', 'flask_blueprint'],  # arbitrary keywords
-    install_requires=REQUIREMENTS,
+    keywords=['flask', 'clask_app_core'],  # arbitrary keywords
+    install_requires=['flask', 'flask-blueprint'],
     entry_points=None
 )
