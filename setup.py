69d2e77f22f6c5c6b6b78c3b2406ad459c936338
Jeffrey Marvin Forones <aiscenblue@gmail.com>
2017-10-28 19:23:38 +0800

    content update

--- setup.py
+++ setup.py
@@ -2,13 +2,13 @@ from distutils.core import setup
 
 setup(
     name='flask-app-core',
-    version='1.0.2',
+    version='1.0.3',
     description='Flask app core',
     author='Jeffrey Marvin Forones',
     author_email='aiscenblue@gmail.com',
     url='https://github.com/aiscenblue/flask-app-core',
     packages=['flask_app_core'],
     keywords=['flask', 'clask_app_core'],  # arbitrary keywords
-    install_requires=['flask', 'flask-blueprint'],
+    install_requires=['flask', 'flask-app-core', 'flask-application', 'flask-app-builder'],
     entry_points=None
 )
