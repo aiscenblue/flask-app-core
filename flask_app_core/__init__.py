cf8b7085dbdf40248e406bc553fcd6fe8176b359
Jeffrey Marvin Forones <aiscenblue@gmail.com>
2017-10-29 19:04:49 +0800

    change parameter for more relevant name

--- flask_app_core/__init__.py
+++ flask_app_core/__init__.py
@@ -10,21 +10,28 @@ class Bootstrap:
     __app = None
     __root_dir = None
     _config = Config
-    _module_dir = ".module"
+    _module_dir = "module"
 
     def __init__(self, import_name, app_dir, **kwargs):
+
         self.__root_dir = os_path.dirname(app_dir)
+
         self.__app = Flask(import_name, instance_relative_config=True)
+
         if "config" in kwargs:
+
             if inspect.isclass(kwargs['config']):
                 self._config = kwargs["config"]
             else:
                 raise ValueError("config must be a class type")
+
         if "module" in kwargs:
+
             if os_path.isdir(kwargs['module']):
                 self._module_dir = kwargs["module"]
             else:
                 raise ValueError("module is not a directory")
+
         if "environment" in kwargs:
             self.set_environment(kwargs['environment'])
 
@@ -55,6 +62,6 @@ class Bootstrap:
 
     def start(self):
         """ for blueprint registration """
-        Core(app=self.__app, root_path=self._module_dir)
+        Core(app=self.__app, root_paths=[self.__root_dir + '/' + self._module_dir])
         self.__app.run(host=self._config.HOST, port=self._config.PORT)
         return self.__app
