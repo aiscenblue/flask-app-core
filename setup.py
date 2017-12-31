7a3301710ea55013cd79c9da91730aca0d51841c
Jeffrey Marvin Forones <aiscenblue@gmail.com>
2017-12-31 19:45:23 +0800

    enhancement

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from distutils.core import setup
 
 setup(
     name='flask-app-core',
-    version='1.2.7',
+    version='1.2.8',
     description='Flask app core',
     author='Jeffrey Marvin Forones',
     author_email='aiscenblue@gmail.com',
