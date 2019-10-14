62cea92bedcec8baa1caf5c36d5f36b39244e81a
Jeffrey Marvin C. Forones <aiscenblue@gmail.com>
2019-10-14 18:11:06 +0800

    Update __init__.py
    bug fixes for the deprecated Core class and added default values on init parameters

--- flask_app_core/__init__.py
+++ flask_app_core/__init__.py
@@ -1,6 +1,6 @@
 import os
 from flask import Flask
-from flask_blueprint import Core
+from flask_blueprint import Blueprint
 from .base_config import DevelopmentConfig as Config
 import inspect
 
@@ -24,7 +24,6 @@ import inspect
 
 
 class Bootstrap:
-
     __app = None
     __root_dir = None
     _config = Config
@@ -36,29 +35,28 @@ class Bootstrap:
     template_folder = _module_dir
     instance_path = None
 
-    def __init__(self, import_name, app_dir, **kwargs):
+    def __init__(self, import_name=__name__, app_dir=__file__, **kwargs):
 
         self.__root_dir = os.path.dirname(os.path.realpath(app_dir))
 
         for key, value in kwargs.items():
             setattr(self, key, value)
 
-        self.__app = Flask(import_name,
-                           instance_relative_config=self.instance_relative_config,
-                           static_url_path=self.static_url_path,
-                           static_folder=self.static_folder,
-                           template_folder=self.template_folder,
-                           instance_path=self.instance_path)
+        self.__app = Flask(
+            import_name,
+            instance_relative_config=self.instance_relative_config,
+            static_url_path=self.static_url_path,
+            static_folder=self.static_folder,
+            template_folder=self.template_folder,
+            instance_path=self.instance_path)
 
         if "config" in kwargs:
-
             if inspect.isclass(kwargs['config']):
                 self._config = kwargs["config"]
             else:
                 raise ValueError("config must be a class type")
 
         if "module" in kwargs:
-
             if os.path.isdir(kwargs['module']):
                 self._module_dir = kwargs["module"]
             else:
@@ -77,23 +75,25 @@ class Bootstrap:
             python 3
                 https://docs.python.org/3/tutorial/modules.html
     """
+
     def set_environment(self, environment_name='development'):
         debug_testing_conf = True
-        setattr(self._config, 'DEBUG', debug_testing_conf)
-        setattr(self._config, 'TESTING', debug_testing_conf)
 
         """ reserved string value for environment name """
-        if environment_name == "production":
+        if environment_name is "production":
             debug_testing_conf = False
-            setattr(self._config, 'DEBUG', debug_testing_conf)
-            setattr(self._config, 'TESTING', debug_testing_conf)
+
+        setattr(self._config, 'DEBUG', debug_testing_conf)
 
     def configuration(self, conf):
         """ configuration file fore core module """
         self.__app.config.from_object(conf)
 
+    def __blueprint_registration(self):
+        Blueprint(app=self.__app, root_paths=[self.__root_dir + '/' + self._module_dir])
+
     def start(self, **kwargs):
         """ for blueprint registration """
-        Core(app=self.__app, root_paths=[self.__root_dir + '/' + self._module_dir])
+        self.__blueprint_registration()
         self.__app.run(host=self._config.HOST, port=self._config.PORT, **kwargs)
         return self.__app
